f01ba52d099e33436e31f9ad1ee274bb2a9046fa
Hannah Caling <hgcaling17@gmail.com>
2024-11-26 15:39:31 +0800

    feat: Added Restaurant activity

--- app/src/main/java/com/example/recyclerviewonandroidgetterandsetter/restaurant/RestaurantPage.java
+++ app/src/main/java/com/example/recyclerviewonandroidgetterandsetter/restaurant/RestaurantPage.java
@@ -16,6 +16,7 @@ import java.util.List;
 
 public class RestaurantPage extends AppCompatActivity {
 
+    // Fixed the errors
     EditText etName, etType, etLocation;
     Button btnAddItem;
     RecyclerView rvRestaurants;
