118ea62fcccc922ade1cc3a33d3af8d5a1b15d23
Hannah Caling <hgcaling17@gmail.com>
2024-11-26 15:30:30 +0800

    feat: Added Restaurant Adapter

--- app/src/main/java/com/example/recyclerviewonandroidgetterandsetter/adapter/RestaurantAdapter.java
+++ app/src/main/java/com/example/recyclerviewonandroidgetterandsetter/adapter/RestaurantAdapter.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 public class RestaurantAdapter extends RecyclerView.Adapter<RestaurantAdapter.RestaurantViewHolder> {
 
+    // This is the adapter for the restaurant page
     private List<Restaurants> restaurantList;
 
     public RestaurantAdapter(List<Restaurants> restaurantList) {
